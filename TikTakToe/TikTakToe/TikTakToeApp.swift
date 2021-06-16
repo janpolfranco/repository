1a9fd63193c206374e8f24504ab17c42c2f402fd
John Paul Franco <janpolfranco@gmail.com>
2021-06-16 19:05:22 +0800

    Tic-tac-toe

---
+++ TikTakToe/TikTakToe/TikTakToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TikTakToeApp.swift
+//  TikTakToe
+//
+//  Created by John Paul M. Franco on 6/16/21.
+//
+
+import SwiftUI
+
+@main
+struct TikTakToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            TiktaktoeAppView()
+        }
+    }
+}
